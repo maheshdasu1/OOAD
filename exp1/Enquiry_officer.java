14c51c44bf9bf394cf324cd08fd925a49b27b977
maheshdasu1 <124768816+maheshdasu1@users.noreply.github.com>
2023-08-21 13:41:03 +0530

    Add files via upload

---
+++ exp1/Enquiry_officer.java
@@ -0,0 +1,62 @@
+
+
+/**
+ * Class Enquiry_officer
+ */
+public class Enquiry_officer {
+
+  //
+  // Fields
+  //
+
+  private new_class verification_of_details;
+  
+  //
+  // Constructors
+  //
+  public Enquiry_officer () { };
+  
+  //
+  // Methods
+  //
+
+
+  //
+  // Accessor methods
+  //
+
+  /**
+   * Set the value of verification_of_details
+   * @param newVar the new value of verification_of_details
+   */
+  private void setVerification_of_details (new_class newVar) {
+    verification_of_details = newVar;
+  }
+
+  /**
+   * Get the value of verification_of_details
+   * @return the value of verification_of_details
+   */
+  private new_class getVerification_of_details () {
+    return verification_of_details;
+  }
+
+  //
+  // Other methods
+  //
+
+  /**
+   */
+  public void issue_of_pass_port()
+  {
+  }
+
+
+  /**
+   */
+  public void renewal_of_passport()
+  {
+  }
+
+
+}
