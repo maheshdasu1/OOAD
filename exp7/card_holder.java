5659394dd004c4e22da757b46410b585449799fb
maheshdasu1 <124768816+maheshdasu1@users.noreply.github.com>
2023-08-22 13:02:35 +0530

    Add files via upload

---
+++ exp7/card_holder.java
@@ -0,0 +1,79 @@
+
+
+/**
+ * Class card_holder
+ */
+public class card_holder {
+
+  //
+  // Fields
+  //
+
+  private void item_purchased;
+  private void credit_card;
+  
+  //
+  // Constructors
+  //
+  public card_holder () { };
+  
+  //
+  // Methods
+  //
+
+
+  //
+  // Accessor methods
+  //
+
+  /**
+   * Set the value of item_purchased
+   * @param newVar the new value of item_purchased
+   */
+  private void setItem_purchased (void newVar) {
+    item_purchased = newVar;
+  }
+
+  /**
+   * Get the value of item_purchased
+   * @return the value of item_purchased
+   */
+  private void getItem_purchased () {
+    return item_purchased;
+  }
+
+  /**
+   * Set the value of credit_card
+   * @param newVar the new value of credit_card
+   */
+  private void setCredit_card (void newVar) {
+    credit_card = newVar;
+  }
+
+  /**
+   * Get the value of credit_card
+   * @return the value of credit_card
+   */
+  private void getCredit_card () {
+    return credit_card;
+  }
+
+  //
+  // Other methods
+  //
+
+  /**
+   */
+  public void sign_bill()
+  {
+  }
+
+
+  /**
+   */
+  public void give_card()
+  {
+  }
+
+
+}
