e140cca2cd7896516b191a84490fc807edd3dac5
maheshdasu1 <124768816+maheshdasu1@users.noreply.github.com>
2023-08-22 13:10:05 +0530

    Add files via upload

---
+++ exp 10/candidate1.java
@@ -0,0 +1,113 @@
+
+
+/**
+ * Class candidate1
+ */
+public class candidate1 {
+
+  //
+  // Fields
+  //
+
+  private void operation;
+  private void name;
+  private void qulification;
+  private void candidate;
+  
+  //
+  // Constructors
+  //
+  public candidate1 () { };
+  
+  //
+  // Methods
+  //
+
+
+  //
+  // Accessor methods
+  //
+
+  /**
+   * Set the value of operation
+   * @param newVar the new value of operation
+   */
+  private void setOperation (void newVar) {
+    operation = newVar;
+  }
+
+  /**
+   * Get the value of operation
+   * @return the value of operation
+   */
+  private void getOperation () {
+    return operation;
+  }
+
+  /**
+   * Set the value of name
+   * @param newVar the new value of name
+   */
+  private void setName (void newVar) {
+    name = newVar;
+  }
+
+  /**
+   * Get the value of name
+   * @return the value of name
+   */
+  private void getName () {
+    return name;
+  }
+
+  /**
+   * Set the value of qulification
+   * @param newVar the new value of qulification
+   */
+  private void setQulification (void newVar) {
+    qulification = newVar;
+  }
+
+  /**
+   * Get the value of qulification
+   * @return the value of qulification
+   */
+  private void getQulification () {
+    return qulification;
+  }
+
+  /**
+   * Set the value of candidate
+   * @param newVar the new value of candidate
+   */
+  private void setCandidate (void newVar) {
+    candidate = newVar;
+  }
+
+  /**
+   * Get the value of candidate
+   * @return the value of candidate
+   */
+  private void getCandidate () {
+    return candidate;
+  }
+
+  //
+  // Other methods
+  //
+
+  /**
+   */
+  public void verify()
+  {
+  }
+
+
+  /**
+   */
+  public void candidate()
+  {
+  }
+
+
+}
