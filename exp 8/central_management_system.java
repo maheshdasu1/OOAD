f8ed161c2db0327c044443daad1784f72d915bf5
maheshdasu1 <124768816+maheshdasu1@users.noreply.github.com>
2023-08-22 13:03:42 +0530

    Add files via upload

---
+++ exp 8/central_management_system.java
@@ -0,0 +1,117 @@
+
+
+/**
+ * Class central_management_system
+ */
+public class central_management_system {
+
+  //
+  // Fields
+  //
+
+  private void employee_name;
+  private void emp_number;
+  private void details;
+  
+  //
+  // Constructors
+  //
+  public central_management_system () { };
+  
+  //
+  // Methods
+  //
+
+
+  //
+  // Accessor methods
+  //
+
+  /**
+   * Set the value of employee_name
+   * @param newVar the new value of employee_name
+   */
+  private void setEmployee_name (void newVar) {
+    employee_name = newVar;
+  }
+
+  /**
+   * Get the value of employee_name
+   * @return the value of employee_name
+   */
+  private void getEmployee_name () {
+    return employee_name;
+  }
+
+  /**
+   * Set the value of emp_number
+   * @param newVar the new value of emp_number
+   */
+  private void setEmp_number (void newVar) {
+    emp_number = newVar;
+  }
+
+  /**
+   * Get the value of emp_number
+   * @return the value of emp_number
+   */
+  private void getEmp_number () {
+    return emp_number;
+  }
+
+  /**
+   * Set the value of details
+   * @param newVar the new value of details
+   */
+  private void setDetails (void newVar) {
+    details = newVar;
+  }
+
+  /**
+   * Get the value of details
+   * @return the value of details
+   */
+  private void getDetails () {
+    return details;
+  }
+
+  //
+  // Other methods
+  //
+
+  /**
+   */
+  public void leave_taken()
+  {
+  }
+
+
+  /**
+   */
+  public void tax()
+  {
+  }
+
+
+  /**
+   */
+  public void loan()
+  {
+  }
+
+
+  /**
+   */
+  public void salary()
+  {
+  }
+
+
+  /**
+   */
+  public void centeral_management_system()
+  {
+  }
+
+
+}
