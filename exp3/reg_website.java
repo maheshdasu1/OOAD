1a23b5cbc44bc5962b1ca6cbf6260537dd8fea90
maheshdasu1 <124768816+maheshdasu1@users.noreply.github.com>
2023-08-22 12:56:47 +0530

    Add files via upload

---
+++ exp3/reg_website.java
@@ -0,0 +1,130 @@
+
+
+/**
+ * Class reg_website
+ */
+public class reg_website {
+
+  //
+  // Fields
+  //
+
+  private void qulification;
+  private void login_details;
+  private void fee_details;
+  private void exam_dates;
+  private void verification;
+  
+  //
+  // Constructors
+  //
+  public reg_website () { };
+  
+  //
+  // Methods
+  //
+
+
+  //
+  // Accessor methods
+  //
+
+  /**
+   * Set the value of qulification
+   * @param newVar the new value of qulification
+   */
+  private void setQulification (void newVar) {
+    qulification = newVar;
+  }
+
+  /**
+   * Get the value of qulification
+   * @return the value of qulification
+   */
+  private void getQulification () {
+    return qulification;
+  }
+
+  /**
+   * Set the value of login_details
+   * @param newVar the new value of login_details
+   */
+  private void setLogin_details (void newVar) {
+    login_details = newVar;
+  }
+
+  /**
+   * Get the value of login_details
+   * @return the value of login_details
+   */
+  private void getLogin_details () {
+    return login_details;
+  }
+
+  /**
+   * Set the value of fee_details
+   * @param newVar the new value of fee_details
+   */
+  private void setFee_details (void newVar) {
+    fee_details = newVar;
+  }
+
+  /**
+   * Get the value of fee_details
+   * @return the value of fee_details
+   */
+  private void getFee_details () {
+    return fee_details;
+  }
+
+  /**
+   * Set the value of exam_dates
+   * @param newVar the new value of exam_dates
+   */
+  private void setExam_dates (void newVar) {
+    exam_dates = newVar;
+  }
+
+  /**
+   * Get the value of exam_dates
+   * @return the value of exam_dates
+   */
+  private void getExam_dates () {
+    return exam_dates;
+  }
+
+  /**
+   * Set the value of verification
+   * @param newVar the new value of verification
+   */
+  private void setVerification (void newVar) {
+    verification = newVar;
+  }
+
+  /**
+   * Get the value of verification
+   * @return the value of verification
+   */
+  private void getVerification () {
+    return verification;
+  }
+
+  //
+  // Other methods
+  //
+
+  /**
+   */
+  public void processing()
+  {
+  }
+
+
+  /**
+   */
+  public void registration()
+  {
+  }
+
+
+}
---
+++ exp3/stduent.java
@@ -0,0 +1,130 @@
+
+
+/**
+ * Class stduent
+ */
+public class stduent {
+
+  //
+  // Fields
+  //
+
+  private void name;
+  private void address;
+  private void DOB;
+  private void subjects;
+  private void collage;
+  
+  //
+  // Constructors
+  //
+  public stduent () { };
+  
+  //
+  // Methods
+  //
+
+
+  //
+  // Accessor methods
+  //
+
+  /**
+   * Set the value of name
+   * @param newVar the new value of name
+   */
+  private void setName (void newVar) {
+    name = newVar;
+  }
+
+  /**
+   * Get the value of name
+   * @return the value of name
+   */
+  private void getName () {
+    return name;
+  }
+
+  /**
+   * Set the value of address
+   * @param newVar the new value of address
+   */
+  private void setAddress (void newVar) {
+    address = newVar;
+  }
+
+  /**
+   * Get the value of address
+   * @return the value of address
+   */
+  private void getAddress () {
+    return address;
+  }
+
+  /**
+   * Set the value of DOB
+   * @param newVar the new value of DOB
+   */
+  private void setDOB (void newVar) {
+    DOB = newVar;
+  }
+
+  /**
+   * Get the value of DOB
+   * @return the value of DOB
+   */
+  private void getDOB () {
+    return DOB;
+  }
+
+  /**
+   * Set the value of subjects
+   * @param newVar the new value of subjects
+   */
+  private void setSubjects (void newVar) {
+    subjects = newVar;
+  }
+
+  /**
+   * Get the value of subjects
+   * @return the value of subjects
+   */
+  private void getSubjects () {
+    return subjects;
+  }
+
+  /**
+   * Set the value of collage
+   * @param newVar the new value of collage
+   */
+  private void setCollage (void newVar) {
+    collage = newVar;
+  }
+
+  /**
+   * Get the value of collage
+   * @return the value of collage
+   */
+  private void getCollage () {
+    return collage;
+  }
+
+  //
+  // Other methods
+  //
+
+  /**
+   */
+  public void form_filling()
+  {
+  }
+
+
+  /**
+   */
+  public void registration()
+  {
+  }
+
+
+}
