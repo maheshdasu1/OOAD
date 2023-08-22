4b51b1fcfa5006e5c03e9369487192da85fe084c
maheshdasu1 <124768816+maheshdasu1@users.noreply.github.com>
2023-08-22 13:01:18 +0530

    Add files via upload

---
+++ exp6/passenger_1.java
@@ -0,0 +1,96 @@
+
+
+/**
+ * Class passenger_1
+ */
+public class passenger_1 {
+
+  //
+  // Fields
+  //
+
+  private void passenger_name;
+  private void passenger_age;
+  private void train_number_;
+  
+  //
+  // Constructors
+  //
+  public passenger_1 () { };
+  
+  //
+  // Methods
+  //
+
+
+  //
+  // Accessor methods
+  //
+
+  /**
+   * Set the value of passenger_name
+   * @param newVar the new value of passenger_name
+   */
+  private void setPassenger_name (void newVar) {
+    passenger_name = newVar;
+  }
+
+  /**
+   * Get the value of passenger_name
+   * @return the value of passenger_name
+   */
+  private void getPassenger_name () {
+    return passenger_name;
+  }
+
+  /**
+   * Set the value of passenger_age
+   * @param newVar the new value of passenger_age
+   */
+  private void setPassenger_age (void newVar) {
+    passenger_age = newVar;
+  }
+
+  /**
+   * Get the value of passenger_age
+   * @return the value of passenger_age
+   */
+  private void getPassenger_age () {
+    return passenger_age;
+  }
+
+  /**
+   * Set the value of train_number_
+   * @param newVar the new value of train_number_
+   */
+  private void setTrain_number_ (void newVar) {
+    train_number_ = newVar;
+  }
+
+  /**
+   * Get the value of train_number_
+   * @return the value of train_number_
+   */
+  private void getTrain_number_ () {
+    return train_number_;
+  }
+
+  //
+  // Other methods
+  //
+
+  /**
+   */
+  public void login()
+  {
+  }
+
+
+  /**
+   */
+  public void passenger()
+  {
+  }
+
+
+}
---
+++ exp6/railway_reservation_system.java
@@ -0,0 +1,110 @@
+
+
+/**
+ * Class railway_reservation_system
+ */
+public class railway_reservation_system {
+
+  //
+  // Fields
+  //
+
+  private void train_number_;
+  private void train_name;
+  private void passenger_name;
+  
+  //
+  // Constructors
+  //
+  public railway_reservation_system () { };
+  
+  //
+  // Methods
+  //
+
+
+  //
+  // Accessor methods
+  //
+
+  /**
+   * Set the value of train_number_
+   * @param newVar the new value of train_number_
+   */
+  private void setTrain_number_ (void newVar) {
+    train_number_ = newVar;
+  }
+
+  /**
+   * Get the value of train_number_
+   * @return the value of train_number_
+   */
+  private void getTrain_number_ () {
+    return train_number_;
+  }
+
+  /**
+   * Set the value of train_name
+   * @param newVar the new value of train_name
+   */
+  private void setTrain_name (void newVar) {
+    train_name = newVar;
+  }
+
+  /**
+   * Get the value of train_name
+   * @return the value of train_name
+   */
+  private void getTrain_name () {
+    return train_name;
+  }
+
+  /**
+   * Set the value of passenger_name
+   * @param newVar the new value of passenger_name
+   */
+  private void setPassenger_name (void newVar) {
+    passenger_name = newVar;
+  }
+
+  /**
+   * Get the value of passenger_name
+   * @return the value of passenger_name
+   */
+  private void getPassenger_name () {
+    return passenger_name;
+  }
+
+  //
+  // Other methods
+  //
+
+  /**
+   */
+  public void status()
+  {
+  }
+
+
+  /**
+   */
+  public void reservation()
+  {
+  }
+
+
+  /**
+   */
+  public void cancellation()
+  {
+  }
+
+
+  /**
+   */
+  public void railway_resevation()
+  {
+  }
+
+
+}
