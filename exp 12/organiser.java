fd298d76013136423b73bfdf09d153b38c28862a
maheshdasu1 <124768816+maheshdasu1@users.noreply.github.com>
2023-08-22 13:56:01 +0530

    Add files via upload

---
+++ exp 12/organiser.java
@@ -0,0 +1,86 @@
+
+
+/**
+ * Class organiser
+ */
+public class organiser {
+
+  //
+  // Fields
+  //
+
+  private void member_details;
+  private void functionl_details;
+  
+  //
+  // Constructors
+  //
+  public organiser () { };
+  
+  //
+  // Methods
+  //
+
+
+  //
+  // Accessor methods
+  //
+
+  /**
+   * Set the value of member_details
+   * @param newVar the new value of member_details
+   */
+  private void setMember_details (void newVar) {
+    member_details = newVar;
+  }
+
+  /**
+   * Get the value of member_details
+   * @return the value of member_details
+   */
+  private void getMember_details () {
+    return member_details;
+  }
+
+  /**
+   * Set the value of functionl_details
+   * @param newVar the new value of functionl_details
+   */
+  private void setFunctionl_details (void newVar) {
+    functionl_details = newVar;
+  }
+
+  /**
+   * Get the value of functionl_details
+   * @return the value of functionl_details
+   */
+  private void getFunctionl_details () {
+    return functionl_details;
+  }
+
+  //
+  // Other methods
+  //
+
+  /**
+   */
+  public void allocating_the_seats()
+  {
+  }
+
+
+  /**
+   */
+  public void lnviting_the_delegates()
+  {
+  }
+
+
+  /**
+   */
+  public void choosing_the_winner()
+  {
+  }
+
+
+}
