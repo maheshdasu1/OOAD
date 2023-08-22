7bd686d8387e374e3cca40b910c3fd81b1657a0f
maheshdasu1 <124768816+maheshdasu1@users.noreply.github.com>
2023-08-22 13:11:04 +0530

    Add files via upload

---
+++ exp 11/central_management_system.java
@@ -0,0 +1,103 @@
+
+
+/**
+ * Class central_management_system
+ */
+public class central_management_system {
+
+  //
+  // Fields
+  //
+
+  private void member_details;
+  private void functionl_details;
+  private void details_of_seat_allocation;
+  
+  //
+  // Constructors
+  //
+  public central_management_system () { };
+  
+  //
+  // Methods
+  //
+
+
+  //
+  // Accessor methods
+  //
+
+  /**
+   * Set the value of member_details
+   * @param newVar the new value of member_details
+   */
+  private void setMember_details (void newVar) {
+    member_details = newVar;
+  }
+
+  /**
+   * Get the value of member_details
+   * @return the value of member_details
+   */
+  private void getMember_details () {
+    return member_details;
+  }
+
+  /**
+   * Set the value of functionl_details
+   * @param newVar the new value of functionl_details
+   */
+  private void setFunctionl_details (void newVar) {
+    functionl_details = newVar;
+  }
+
+  /**
+   * Get the value of functionl_details
+   * @return the value of functionl_details
+   */
+  private void getFunctionl_details () {
+    return functionl_details;
+  }
+
+  /**
+   * Set the value of details_of_seat_allocation
+   * @param newVar the new value of details_of_seat_allocation
+   */
+  private void setDetails_of_seat_allocation (void newVar) {
+    details_of_seat_allocation = newVar;
+  }
+
+  /**
+   * Get the value of details_of_seat_allocation
+   * @return the value of details_of_seat_allocation
+   */
+  private void getDetails_of_seat_allocation () {
+    return details_of_seat_allocation;
+  }
+
+  //
+  // Other methods
+  //
+
+  /**
+   */
+  public void storing()
+  {
+  }
+
+
+  /**
+   */
+  public void update_details()
+  {
+  }
+
+
+  /**
+   */
+  public void central_management_system()
+  {
+  }
+
+
+}
