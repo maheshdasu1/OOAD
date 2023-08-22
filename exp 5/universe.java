43537b15c5e3ab548835a4c5e6c670cb41bc862e
maheshdasu1 <124768816+maheshdasu1@users.noreply.github.com>
2023-08-22 12:59:18 +0530

    Add files via upload

---
+++ exp 5/universe.java
@@ -0,0 +1,62 @@
+
+
+/**
+ * Class universe
+ */
+public class universe {
+
+  //
+  // Fields
+  //
+
+  private void verify_details;
+  
+  //
+  // Constructors
+  //
+  public universe () { };
+  
+  //
+  // Methods
+  //
+
+
+  //
+  // Accessor methods
+  //
+
+  /**
+   * Set the value of verify_details
+   * @param newVar the new value of verify_details
+   */
+  private void setVerify_details (void newVar) {
+    verify_details = newVar;
+  }
+
+  /**
+   * Get the value of verify_details
+   * @return the value of verify_details
+   */
+  private void getVerify_details () {
+    return verify_details;
+  }
+
+  //
+  // Other methods
+  //
+
+  /**
+   */
+  public void store_details()
+  {
+  }
+
+
+  /**
+   */
+  public void enroll()
+  {
+  }
+
+
+}
