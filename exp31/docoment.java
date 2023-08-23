dff3216b8f7b520728f3ef34b5875066147badad
maheshdasu1 <124768816+maheshdasu1@users.noreply.github.com>
2023-08-23 09:37:24 +0530

    Add files via upload

---
+++ exp31/docoment.java
@@ -0,0 +1,31 @@
+
+
+/**
+ * Class docoment
+ */
+public class docoment {
+
+  //
+  // Fields
+  //
+
+  
+  //
+  // Constructors
+  //
+  public docoment () { };
+  
+  //
+  // Methods
+  //
+
+
+  //
+  // Accessor methods
+  //
+
+  //
+  // Other methods
+  //
+
+}
