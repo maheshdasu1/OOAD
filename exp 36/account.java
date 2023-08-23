05c00770c2337b4a919f8205cef2e38550994d2b
maheshdasu1 <124768816+maheshdasu1@users.noreply.github.com>
2023-08-23 10:25:42 +0530

    Add files via upload

---
+++ exp 36/account.java
@@ -0,0 +1,72 @@
+
+
+/**
+ * Class account
+ */
+public class account {
+
+  //
+  // Fields
+  //
+
+  private void account_id;
+  private void account_type;
+  
+  //
+  // Constructors
+  //
+  public account () { };
+  
+  //
+  // Methods
+  //
+
+
+  //
+  // Accessor methods
+  //
+
+  /**
+   * Set the value of account_id
+   * @param newVar the new value of account_id
+   */
+  private void setAccount_id (void newVar) {
+    account_id = newVar;
+  }
+
+  /**
+   * Get the value of account_id
+   * @return the value of account_id
+   */
+  private void getAccount_id () {
+    return account_id;
+  }
+
+  /**
+   * Set the value of account_type
+   * @param newVar the new value of account_type
+   */
+  private void setAccount_type (void newVar) {
+    account_type = newVar;
+  }
+
+  /**
+   * Get the value of account_type
+   * @return the value of account_type
+   */
+  private void getAccount_type () {
+    return account_type;
+  }
+
+  //
+  // Other methods
+  //
+
+  /**
+   */
+  public void get_balance()
+  {
+  }
+
+
+}
